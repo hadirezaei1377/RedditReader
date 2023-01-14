ec698da759a1f5e27e533d64c6da2ba212ce50c9
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2023-01-14 17:52:31 +0330

    code

--- main.go
+++ main.go
@@ -1,1 +1,61 @@
 package main
+
+/*
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/jzelinskie/geddit"
+)
+
+// recieve data from Reddit by an object named redditSession
+// Its type is a type of pointer to LoginSession
+// This object must be set in order to connect to the site
+var redditSession *geddit.LoginSession
+
+// story is news ,at the moment we just need title and url
+type Story struct {
+	title string
+	url   string
+}
+
+func init() {
+	var err error
+	redditSession, err = geddit.NewLoginSession("g_d_bot", "K417k4FTua52", "gdAgent v0")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func main() {
+
+	stories := loadReddit()
+	fmt.Printf("%v \n\n", stories)
+
+}
+
+// function for calling Reddit site and recive data from it
+func loadReddit() []Story {
+	var stories []Story
+
+	var listOption geddit.ListingOptions
+	submission, err := redditSession.SubredditSubmissions("programming", "", listOption)
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+
+	for _, s := range submission {
+		story := Story{
+			title: s.Title,
+			url:   s.URL,
+		}
+		stories = append(stories, story)
+	}
+
+	return stories
+}
+
+*/
--- readme.md
+++ readme.md
@@ -6,9 +6,12 @@ any things which our program reads named input
 Output :
 any things which our program shows named output
 
-we want to recieve news from Reddit site and print in terminal and copy them in a file 
+we want to recieve news from Reddit site and print in terminal and copy them in a file by using Reddit API
 
+we are reading data from a site by network(our inputs) then see and print it (our outputs)
 
-
+g_d_bot is username test
+K417k4FTua52 is password test
+gdAgent is agent test (version which API using now)
 
 
